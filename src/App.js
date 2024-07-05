2345b12e06ebaca0362cdddbf8dd9b514308cabf
mccartymv <mvmccart@gmail.com>
2024-07-05 14:43:32 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React, { useRef, useEffect } from 'react';
+import mapboxgl from 'mapbox-gl';
 import './App.css';
 
-function App() {
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWJhZG5hcjQ1IiwiYSI6ImNseTkwMWQ4cDBrcG8yanBuNmV4ZnJiZDQifQ.EkfYeLvS7KYopWt5ULdZ9g';
+
+const App = () => {
+  const mapContainerRef = useRef(null);
+
+  useEffect(() => {
+    // Initialize map
+    const map = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      style: 'mapbox://styles/mapbox/dark-v10', // Map style
+      center: [136, 38.5], // Initial center [lng, lat]
+      zoom: 4.5 // Initial zoom
+    });
+
+    // Add navigation control (zoom buttons)
+    map.addControl(new mapboxgl.NavigationControl(), 'top-right');
+
+    // Clean up on unmount
+    return () => map.remove();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="map-container" ref={mapContainerRef} style={{ width: '100%', height: '100vh' }} />
     </div>
   );
-}
+};
 
 export default App;
